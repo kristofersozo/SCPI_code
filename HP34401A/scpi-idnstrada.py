35ffa94f100806740285652d2e0d6cbc8b75e654
kristofersozo <127208485+kristofersozo@users.noreply.github.com>
2024-02-12 12:07:56 +0200

    Update scpi-idnstrada.py

--- HP34401A/scpi-idnstrada.py
+++ HP34401A/scpi-idnstrada.py
@@ -1,26 +1,26 @@
 import serial	# pakete priekš komunikācijas ar virknes interfeisu
 import time	# pakete priekš ar laiku saistītām operācijām
 
-ser = serial.Serial('COM4', 9600)	# atver COM portu
+ser = serial.Serial('COM4', 9600)	# atver komunikācijas portu
 ser.write(b'\n')	# izveido jaunu rindu, lai pārliecinātos, ka visi neapstiprinātie dati ir notīrīti
-ser.flush()	# iztīra autputa buferi, lai pārliecinātos, ka dati ir nosūtīti
+ser.flush()	# iztīra izejas buferi, lai pārliecinātos, ka dati ir nosūtīti
 time.sleep(1)	# nogaida 1 sekundi
 ser.read_all()	# nolasa visus pieejamos datus no bufera
 ser.write(b'*RST\n')	# nosūta "reset" komandu
-ser.flush()	# iztīra autputa buferi, lai pārliecinātos, ka dati ir nosūtīti
+ser.flush()	# iztīra izejas buferi, lai pārliecinātos, ka dati ir nosūtīti
 print('Resetting the device...')
 time.sleep(1)	# nogaida 1 sekundi
 ser.write(b'*CLS\n')	# nosūta komandu, kas iztīra ierīci
-ser.flush()	# iztīra autputa buferi, lai pārliecinātos, ka dati ir nosūtīti
+ser.flush()	# iztīra izejas buferi, lai pārliecinātos, ka dati ir nosūtīti
 print('Clearing the device...')
 time.sleep(1)	# nogaida 1 sekundi
-#ser.timeout = 5000	# virknes interfeisa komunikācijas taimauts milisekundēs
+#ser.timeout = 5000	# virknes interfeisa komunikācijas pārtraukums milisekundēs
 
 #ser.write(b'*RST;*CLS\n')	# vienlaicīgu nosūta "reset" un komandu, kas iztīra ierīci
 while True:	# izveido atkārtojošos cilpu
 	 # nosūta "*IDN?" komandu, lai noteiktu ierīces identifikāciju
 	ser.write(b'*IDN?\n')
-	ser.flush()	# iztīra autputa buferi, lai pārliecinātos, ka dati ir nosūtīti
+	ser.flush()	# iztīra izejas buferi, lai pārliecinātos, ka dati ir nosūtīti
 
 	print('Sending the "*IDN?" command...')
 
@@ -39,4 +39,4 @@ while 1:	# izveido atkārtojošos cilpu
 	print('writing')
 	ser.write(b'*IDN?\n')	# nosūta "*IDN?" komandu, lai noteiktu ierīces identifikāciju
 
-ser.close()	# aizver COM portu
+ser.close()	# aizver komunikācijas portu
